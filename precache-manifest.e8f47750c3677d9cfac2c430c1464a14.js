792f409354fd6cc48955d54a631e85dd650edcf8
Dion Pinto <dionpinto000@gmail.com>
2020-08-26 19:24:20 +0530

    Updates

---
+++ precache-manifest.e8f47750c3677d9cfac2c430c1464a14.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3cde01226a85bb7a894a48433290125",
+    "url": "/Automate-git/index.html"
+  },
+  {
+    "revision": "bae84d5541cdfe51d4f7",
+    "url": "/Automate-git/static/css/2.da3a6d41.chunk.css"
+  },
+  {
+    "revision": "7d456936b29c591c9e36",
+    "url": "/Automate-git/static/css/main.0cdd8b35.chunk.css"
+  },
+  {
+    "revision": "bae84d5541cdfe51d4f7",
+    "url": "/Automate-git/static/js/2.6ad38553.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/Automate-git/static/js/2.6ad38553.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d456936b29c591c9e36",
+    "url": "/Automate-git/static/js/main.b53a0e07.chunk.js"
+  },
+  {
+    "revision": "c005e6c0cdae92ee0920",
+    "url": "/Automate-git/static/js/runtime-main.e82b96ba.js"
+  }
+]);
